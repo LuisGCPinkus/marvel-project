10162d6b5ee2931a431f8eea0866fea6a55228fa
luis.castillo <luis.castillo@springlabs.net>
2022-12-28 12:38:36 -0600

    Deleting all js files

--- build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    publicKey: "6e64266920535df42037ac6d9de37081",
-    privateKey: "37e8934723e0d1b3b26ccdc7d7470e28d369af9e"
-};
--- build/test/hello-test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloTest = void 0;
-const helloTest = () => { return true; };
-exports.helloTest = helloTest;
